5c7b079d33e206cba49980268f19cced1125a3c1
jvdbergh <jvandenberghe@isabelgroup.eu>
2020-10-29 11:40:42 +0100

    test: fixing failing test

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,6 +1,15 @@
 public class Bank {
 
 	public Ignod[] exchangeIgnod(Ignod ignodToExchange) {
-		return new Ignod[0];
+		Ignod ignods[]=  new Ignod[3];
+		ignods[0] = ignodDivider(ignodToExchange.getIgnodValue(), 2);
+		ignods[1] = ignodDivider(ignodToExchange.getIgnodValue(), 3);
+		ignods[2] = ignodDivider(ignodToExchange.getIgnodValue(), 4);
+		return ignods;
+	}
+
+	private Ignod ignodDivider(int ignodValue, int div) {
+		int divIgnodValue = Math.floorDiv(ignodValue, div);
+		return new Ignod(divIgnodValue);
 	}
 }
